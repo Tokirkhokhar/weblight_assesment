694c126c993c00898f46404a5585db9ba0f1a0df
Khushi patel <72152231+Tokirkhokhar@users.noreply.github.com>
2022-10-15 22:44:33 +0530

    user API is ready

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,21 @@
 const express = require("express");
 
+const  userRouter  = require("./router/userRouter");
+
+require("./config/db");
+const cors = require('cors');
+
 const app = express();
+
+app.use(cors());
+
+app.set("view engine","ejs");
+app.use(express.json());
+
+app.use("/api/user",userRouter);
+
+app.use("/api",(req,res,next) =>{
+    res.send("hello")
+})
 
 app.listen(5000, () => console.log("app started at 5000..."));
